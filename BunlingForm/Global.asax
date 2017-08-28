8d8e7f48a58d5373efe8f35ebb0e5ce242814d62
NileshSham <shambharkar165@gmail.com>
2017-08-28 14:56:52 +0530

    gfhgn

---
+++ BunlingForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BunlingForm.MvcApplication" Language="C#" %>
---
+++ BunlingForm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BunlingForm.Startup))]
+namespace BunlingForm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
